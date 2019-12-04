45041099f3a0efbae974a36c984ae19132b772cd
hannahscript <h4nn4h777@protonmail.com>
2019-12-04 06:50:22 +0100

    Performance fix and some flaky measuring for day 04

--- day04/solution.hs
+++ day04/solution.hs
@@ -2,6 +2,9 @@ import Common
 import Data.List
 import Data.Char (digitToInt)
 
+import Control.Exception
+import Data.Time
+
 range = [146810..612564]
 
 digits :: Int -> [Int]
@@ -18,16 +21,19 @@ solution1 :: Int
 solution1 = length (filter valid range)
 
 newValid :: Int -> Bool
-newValid password = valid password && any (\g -> (length g) == 2) (group $ digits password)
+newValid password = monotone password && any (\g -> (length g) == 2) (group $ digits password)
 
 solution2 :: Int
 solution2 = length (filter newValid range)
 
 main = do
+    start <- getCurrentTime
     putStrLn "Solution 1: "
     print solution1
     putStrLn "Solution 2: "
     print solution2
+    end <- getCurrentTime
+    print (diffUTCTime end start)
     
 -- Solution 1: 1748
 -- Solution 2: 1180
